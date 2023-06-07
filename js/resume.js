dac4565a86df848d5071c4f1cd4012d91c4a188f
Aryan Bhola <aryanbhola31@gmail.com>
2023-06-07 17:20:11 +0530

    animations

--- js/resume.js
+++ js/resume.js
@@ -1,32 +1,45 @@
-function loadContent() {
-  var projectsSection = document.querySelector('.section.projects');
-  var projectsCards = document.querySelector('.section.projects .cards');
-
-  // Check if the projects section is in view
-  if (isElementInViewport(projectsSection)) {
-    // Perform actions to load content for the projects section
-    var projectCards = Array.from(document.querySelectorAll('.section.projects .card'));
-
-    // Append the project cards to the projects cards container
-    projectCards.forEach(card => {
-      projectsCards.appendChild(card);
-    });
-
-    // Once the content is loaded, remove the event listener
-    window.removeEventListener('scroll', loadContent);
+document.addEventListener('DOMContentLoaded', function() {
+    ScrollReveal().reveal('.cards .card', {
+  delay: 400,
+  distance: '100px',
+  duration: 2000,
+  interval: 500,
+  beforeReveal: function (el) {
+    el.classList.add('active');
   }
-}
+});
 
-// Helper function to check if an element is in the viewport
-function isElementInViewport(element) {
-  var rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
+ScrollReveal().reveal('.projects', {
+  delay: 600,
+  distance: '50px',
+  duration: 1500,
+  easing: 'ease-out',
+  interval: 300,
+  beforeReveal: function (el) {
+    el.classList.add('active');
+  }
+});
 
-// Add the scroll event listener to trigger loading content
-window.addEventListener('scroll', loadContent);
+ScrollReveal().reveal('.skill-card', {
+  delay: 800,
+  distance: '80px',
+  duration: 1800,
+  easing: 'ease-in-out',
+  interval: 400,
+  beforeReveal: function (el) {
+    el.classList.add('active');
+  }
+});
+
+ScrollReveal().reveal('.contact', {
+  delay: 1000,
+  distance: '120px',
+  duration: 2000,
+  easing: 'ease-in-out',
+  interval: 500,
+  beforeReveal: function (el) {
+    el.classList.add('active');
+  }
+});
+
+});
