dc8866d0b38fda5d521d86ead5bbf8803d6693cc
Aryan Bhola <aryanbhola31@gmail.com>
2023-06-07 20:08:26 +0530

    ;

--- js/resume.js
+++ js/resume.js
@@ -1,45 +1,56 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
     ScrollReveal().reveal('.cards .card', {
-  delay: 400,
+  delay: 200,
   distance: '100px',
-  duration: 2000,
-  interval: 500,
+  duration: 1000,
+ // interval: 500,
   beforeReveal: function (el) {
     el.classList.add('active');
   }
 });
-
-ScrollReveal().reveal('.projects', {
-  delay: 600,
-  distance: '50px',
-  duration: 1500,
-  easing: 'ease-out',
-  interval: 300,
+ScrollReveal().reveal('.cards .card .card-buttons', {
+  delay: 200,
+  distance: '100px',
+  duration: 1000,
+  interval: 500,
   beforeReveal: function (el) {
     el.classList.add('active');
   }
 });
 
 ScrollReveal().reveal('.skill-card', {
-  delay: 800,
-  distance: '80px',
-  duration: 1800,
-  easing: 'ease-in-out',
-  interval: 400,
+  delay: 100,
+  distance: '100px',
+  duration: 1000,
+  interval: 500,
   beforeReveal: function (el) {
     el.classList.add('active');
   }
 });
-
-ScrollReveal().reveal('.contact', {
-  delay: 1000,
-  distance: '120px',
-  duration: 2000,
-  easing: 'ease-in-out',
+ScrollReveal().reveal('h1', {
+  delay: 500,
+  distance: '100px',
+  duration: 1000,
+  interval: 500,
+  beforeReveal: function (el) {
+    el.classList.add('active');
+  }
+});
+ScrollReveal().reveal('.flex-list li', {
+  delay: 200,
+  distance: '100px',
+  duration: 1000,
   interval: 500,
   beforeReveal: function (el) {
     el.classList.add('active');
   }
 });
 
+
+
+
 });
+
+      
